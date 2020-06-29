5906da12b551961a60b877f545686a6264789d1f
Jonathan Mills <jonathanfmills@gmail.com>
2020-06-29 10:41:37 -0500

    initial

--- index.js
+++ index.js
@@ -1,54 +1,18 @@
 const { ApolloServer, gql } = require('apollo-server');
 const SessionAPI = require('./datasources/sessions');
 
-const typeDefs = gql`
-  type Query {
-    sessions(
-      id: ID
-      title: String
-      description: String
-      startsAt: String
-      endsAt: String
-      room: String
-      day: String
-      format: String
-      track: String
-      level: String
-    ): [Session]
-    sessionById(id: ID): Session
-  }
-  type Session {
-    id: ID!
-    title: String!
-    description: String
-    startsAt: String
-    endsAt: String
-    room: String
-    day: String
-    format: String
-    track: String
-      @deprecated(
-        reason: "Too many sessions do not fit into a single track, we will be migrating to a tags based system in the future..."
-      )
-    level: String
-  }
-`;
+const typeDefs = require('./schema.js');
 
-const resolvers = {
-  Query: {
-    sessions: (parent, args, { dataSources }, info) => {
-      return dataSources.sessionAPI.getSessions(args);
-    },
-    sessionById: (parent, { id }, { dataSources }, info) => {
-      return dataSources.sessionAPI.getSessionById(id);
-    },
-  },
-};
+const resolvers = require('./resolvers.js');
 
 const dataSources = () => ({
   sessionAPI: new SessionAPI(),
 });
-const server = new ApolloServer({ typeDefs, resolvers, dataSources });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources,
+});
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
   console.log(`graphQL running at ${url}`);
